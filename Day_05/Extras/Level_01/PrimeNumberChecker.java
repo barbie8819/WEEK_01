f76c78ed74627d4e4e9a7877ed6f28cd004a8d03
Anshi Patel <anshipatel881@gmail.com>
2025-01-18 11:41:05 +0530

    Prime Number

---
+++ Day_05/Extras/Level_01/PrimeNumberChecker.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class PrimeNumberChecker {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number to check if it is a prime number: ");
+        int number = scanner.nextInt();
+
+        boolean isPrime = checkPrime(number);
+
+        if (isPrime) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+
+        scanner.close();
+    }
+
+    // Function to check if a number is prime
+    private static boolean checkPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
